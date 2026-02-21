377902441f2736735f687617f4177f88aeee8b02
Edmund Lim <elimcw20@gmail.com>
2026-02-22 02:13:12 +0800

    refactor: Fn logic and JSON into utils folder

--- script.py
+++ script.py
@@ -1,28 +1,9 @@
 import typer
-import yfinance as yf
+from utils.logic import check_stock_movement
+
 
 app = typer.Typer()
 
-def check_stock_movement(symbol: str):
-    ticker = yf.Ticker(symbol)
-    # retrieve historical data for __ period
-    hist = ticker.history(period="5d") # returns a dataframe
-    # print(hist)
-    
-    if hist.empty:
-        typer.echo(f"No data found for {symbol}")
-        return
-
-    price_now = hist['Close'].iloc[-1] # latest price in df
-    price_start = hist['Close'].iloc[0] # oldest price in df
-    
-    percent_change = ((price_now - price_start) / price_start) * 100
-    if percent_change < 0:
-        typer.echo(f"{symbol.upper()} dipped {percent_change:.2f}% over the last 5 trading days.")
-    elif percent_change > 0:
-        typer.echo(f"{symbol.upper()} rose {percent_change:.2f}% over the last 5 trading days.")
-    else:
-        typer.echo(f"{symbol.upper()} had no change over the last 5 trading days.")
 
 #scan for specific ticker
 @app.command()
